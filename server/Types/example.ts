fd7b72ab8a43b8a04ba7ed6c910771ae72f99854
radizorit <jlimanalysis@gmail.com>
2022-11-01 13:41:51 -0700

    backend set up

--- server/Types/example.ts
+++ server/Types/example.ts
@@ -1,11 +1,11 @@
-import exampleController from "../controllers/exampleController"
+// import exampleController from "../controllers/exampleController"
 import { Request, Response, NextFunction } from 'express';
 
-export interface Example {
+export interface ExampleType {
     create: (req: Request, res: Response, next: NextFunction) => void;
     read: (req: Request, res: Response, next: NextFunction) => void;
     update: (req: Request, res: Response, next: NextFunction) => void;
     delete: (req: Request, res: Response, next: NextFunction) => void;
-    
+
 
 }
