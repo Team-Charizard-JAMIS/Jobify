027b8343c13e63a93c7d431129ba96e792753ec6
Ian <ianhnizdo@gmail.com>
2022-11-01 12:12:50 -0400

    Updated SQL Posts

--- server/controllers/SQLDatabase.js
+++ server/controllers/SQLDatabase.js
@@ -26,6 +26,14 @@ jobController.getApplications = async (req, res, next) => {
     const { username, password } = req.query;
 
 const queryString = `SELECT * FROM Users INNER JOIN Applications ON Users.user_id = Applications.user_id WHERE username='${username}' AND password='${password}'`;
+
+const result = await db.query(queryString)
+.then(response => {
+    res.locals.Applications = response
+    return next();
+})
+.catch((err)=> next({log: `jobController.getApplications: ERROR: ${err}`, message: {err: "Error occurred in jobController.getApplications"}}))
+
 }
 
 jobController.getInterviews = (req, res, next) => {
@@ -34,8 +42,12 @@ jobController.getInterviews = (req, res, next) => {
 
 const queryString = `SELECT * FROM Users INNER JOIN Interviews ON Users.user_id = Interviews.user_id WHERE username='${username}' AND password='${password}'`;
 
-const result= await db.query(queryString)
-.then(response => res.locals.Interviews = response)
+const result = db.query(queryString)
+.then(response => {
+    res.locals.Interviews = response
+    return next();
+})
+
 .catch((err) => next({log: `jobController.getInterviews: ERROR: ${err}`,
 message: {err: "Error occurred in jobController.getInterviews"}}))
 }
@@ -45,6 +57,14 @@ jobController.getOffers = (req, res, next) => {
     const { username, password } = req.query;
 
 const queryString = `SELECT * FROM Users INNER JOIN Offers ON Users.user_id = Offers.user_id WHERE username='${username}' AND password='${password}'`;
+
+const result = db.query(queryString)
+.then(response => {
+    res.locals.Offers = response;
+    return next();
+})
+.catch((err) => next({log : `jobController.getOffers: ERROR: ${err}`, message: {err: "Error occurred in jobController.getOffers"}}))
+
 }
 
 jobController.getAllInfo = async (req, res, next) => {
@@ -53,32 +73,75 @@ jobController.getAllInfo = async (req, res, next) => {
 
     const queryString = `SELECT * FROM Users INNER JOIN Applications ON Users.user_id = Applications.user_id INNER JOIN Interviews ON Users.user_id=Interviews.user_id INNER JOIN Offers ON Users.user_id = Offers.user_id WHERE username='${username}' AND password='${password}'`;
 
-    const result = await db.query(queryString);
-
+    const result = await db.query(queryString)
+    .then(response => {
+        res.locals.Info = response;
+        return next();
+    })
+    .catch((err)=>  next({log : `jobController.getAllInfo: ERROR: ${err}`, message: {err: "Error occurred in jobController.getAllInfo"}}))
 
 }
 
 jobController.postNewUser = (req, res, next) => {
-const queryString = `INSERT INTO Users(username, password) VALUES(${req.body.username}, ${req.body.password})`
-db.query(queryString)
-.then((results) => console.log(results))
+
+    const { username, password } = req.query;
+
+    const queryString = `INSERT INTO Users(username, password) VALUES('${req.body.username}', '${req.body.password}')`
+
+    db.query(queryString)
+.then((results) => {
+    console.log(results)
+    return next();
+})
 .catch(err => next({log : `jobController.postNewUser err ${JSON.stringify(err)}`, message : 'Error'}))
+
 }    
 
 jobController.postApplication = (req, res, next) => {
-const queryString = `INSERT INTO  Applications (appName, appDate, user_id) VALUES (${req.body.appName}, ${req.body.appDate}, ${req.body.user_id})`;
+
+    const { username, password } = req.query;
+
+    const queryString = `INSERT INTO Applications (appName, appDate, user_id) VALUES ('${req.body.appName}', '${req.body.appDate}', (SELECT user_id FROM Users WHERE username='${username}' AND password='${password}'))`;
 
 db.query(query)
-.then((results)=>)
+.then((results)=>{
+    console.log('postApplication results', results);
+    return next();
+})
+.catch(err => next({log : `jobController.postApplication err ${JSON.stringify(err)}`, message : 'Error'}))
 
 }
 
 jobController.postInterview = (req, res, next) => {
-const queryString = `INSERT INTO Interviews (interviewName, interviewDate, user_id) VALUES (${req.body.interviewName}, ${req.body.interviewDate}, ${req.body.user_id})`;
+
+    const { username, password } = req.query;
+
+    //goal: find a way to insert into another table based on the username and password of Users
+    const queryString = `INSERT INTO Interviews (interviewName, interviewDate, user_id) VALUES ('${req.body.interviewName}', '${req.body.interviewDate}', (SELECT user_id FROM Users WHERE username='${username}' AND password='${password}'))`;
+
+    db.query(queryString)
+    .then((results)=> {
+        console.log('postInterview results,', results)
+        return next();
+        })
+    .catch(err =>  next({log : `jobController.postInterview err ${JSON.stringify(err)}`, message : 'Error'}))
+    
 }
 
 jobController.postOffer = (req, res, next) => {
-const queryString = `INSERT INTO Offers (offerName, offerDate, user_id) VALUES (${req.body.offerName}, ${req.body.offerDate}, ${req.body.user_id})`;
+
+    const { username, password } = req.query;
+
+    const queryString = `INSERT INTO Offers (offerName, offerDate, user_id) VALUES ('${req.body.offerName}', '${req.body.offerDate}', (SELECT user_id FROM Users WHERE username='${username}' AND password='${password}'))`;
+
+    db.query(queryString)
+    .then((results)=> {
+        console.log('postOffer results,', results)
+        return next();
+        })
+    .catch((err => next({log : `jobController.postOffer err ${JSON.stringify(err)}`, message : 'Error'})))
+
+
 }
 
 module.exports = jobController;
