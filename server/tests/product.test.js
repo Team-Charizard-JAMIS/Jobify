021157296bc3d0f915bb9917b8e7c3d2bc3a3725
radizorit <jlimanalysis@gmail.com>
2022-11-02 12:55:51 -0700

    testing

---
+++ server/tests/product.test.js
@@ -0,0 +1,12 @@
+const request = require("supertest")
+const server = require('../server');
+const db = require('../models/SQLModel');
+/* Connecting to the database before each test. */
+beforeEach(async () => {
+    await mongoose.connect(process.env.MONGODB_URI);
+});
+
+/* Closing database connection after each test. */
+afterEach(async () => {
+    await mongoose.connection.close();
+});
